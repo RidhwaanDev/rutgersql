48b5986c53f9dd08d3d05915b202d20c9e5435ce
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2019-08-01 11:09:40 -0400

    save functions

--- src/resolvers.js
+++ src/resolvers.js
@@ -5,36 +5,60 @@ const chalk = require('chalk');
 const log = console.log;
 // resolver functions for transloc graphql api. Returns an object {} with functions as properties
 const resolvers = {
-    getArrivals: function(args,context){
-        log(chalk.blue(args));
-        getRoutes();
+    arrivals: function(args,context){
     },
-    getRoutes: function(args, context){
+    routes: function(args, context){
+        return getRoutes();
+    },
+    segments: function(args,context){
 
     },
-    getSegments: function(args,context){
+    vehicles: function(args,context){
 
     },
-    getVehicles: function(args,context){
-
-    },
-    getStops: function(args,context){
+    stops: function(args,context){
 
     },
 };
 
 function getRoutes(){
     const URL = config.API_URL + '/routes.json';
-    axios.get(URL, {'headers': config.HEADERS})
+    axios.get(URL, {
+        headers : config.HEADERS,
+        params : {
+        'agencies' : '1323',
+        'geo_area'  : config.geo_area,
+        }})
         .then((res) => {
             log(chalk.bgGreen.black('Success'));
+            log(res['data']);
             return res['data'];
         })
         .catch((error) => {
-            log(chalk.bgRed.black('Error'));
-            log(chalk.yellow(JSON.stringify(error.type)));
-
-        })
+                // https://gist.github.com/fgilio/230ccd514e9381fafa51608fcf137253
+                const pf = chalk.bgRed.black;
+                log(pf("ERROR!!!!"));
+                if (error.response) {
+                    /*
+                     * The request was made and the server responded with a
+                     * status code that falls out of the range of 2xx
+                     */
+                    log(error.response.data);
+                    log(error.response.status);
+                    log(error.response.headers);
+                } else if (error.request) {
+                    /*
+                     * The request was made but no response was received, `error.request`
+                     * is an instance of XMLHttpRequest in the browser and an instance
+                     * of http.ClientRequest in Node.js
+                     */
+                    log(error.request);
+                } else {
+                    // Something happened in setting up the request and triggered an Error
+                    log('Error', error.message);
+                }
+                log(error.config);
+            });
 }
 
 module.exports = resolvers;
--- src/schema.js
+++ src/schema.js
@@ -3,25 +3,25 @@ const { buildSchema } = require('graphql');
 const schema = buildSchema(`
 
 	type Query {
-		routes(campus : String!) : RouteResult,
+		routes : RouteResult,
 	}
 
 	type RouteResult {
-		rate_limit : Int,
-		expires_in: Int,
-		api_latest_version: String,
-		generated_on: String,
-		data : [Route]
+        rate_limit: Int,
+        expires_in: Int,
+        api_latest_version: String,
+        generated_on: String,
+        # data: [Route],
+        api_version: String
 	}
 
 	type Route {
 		route_id : String!,
 		color : String!,
+		isActive : Boolean!,
 		text_color : String!,
 		long_name : String!,
-		# stops : [Stop]
 	}
-
 `);
 
 module.exports = schema;
