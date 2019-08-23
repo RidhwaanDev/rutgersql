1cdd68bcf6b1c8c828a570c88ddaa68e2ca8ad18
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2019-08-23 13:43:02 -0400

    add cache

--- src/cache.js
+++ src/cache.js
@@ -1,10 +1,39 @@
-// cache API data from Transloc
+/**
+ cache API data from Transloc
+ cache a map of stop_id's to names
+ cache a map of route_id to route_names
+ cache all active vehicles
+ cache all active routes
+ update cache every x seconds
+ **/
 
-// cache a map of stop_id's to names
-// cache a map of route_id to route_names
-// cache all active vehicles
-// cache all active routes
-// update cache every x seconds
+const NodeCache = require( "node-cache" );
+
+const log = console.log;
+
+function Cache(checkPeriod, deleteOnExpire, errorOnMissing) {
+    const myCache = new NodeCache();
+    this.store = (obj) => {
+        myCache.set("myKey", obj, (err, success) => {
+            if (!err && success) {
+                log(success);
+            }
+        });
+    };
+
+    this.get = (value) => {
+        myCache.get("myKey", (err, value) => {
+            if (err) {
+                return;
+            }
+            if (value === undefined) {
+                log("Not found")
+            } else {
+            }
+        });
+    };
+}
+module.exports = Cache;
 
 
 
