f3adff44df37674445aefa796b2efbe9fc2b87d3
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2019-09-06 11:58:50 -0400

    fixed it

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // this is used for local testing. ( i.e. node server.js )
-import app from './src/app';
-const port = 1337;
+const app = require('./src/app');
+port = 1337;
 
 app.listen(
     port,  () =>  console.log('GraphQL Server Running at http://127.0.0.1:' + port )
--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-import express from 'express'
-import chalk from 'chalk'
-import graphqlHTTP from "express-graphql";
-import schema from './schema'
-import root from './resolvers'
-import cache from './cache'
+const express  = require('express');
+const chalk  = require('chalk');
+const graphqlHTTP  = require("express-graphql");
+const schema  = require('./schema');
+const root  = require( './resolvers');
+const cache  = require('./cache');
 
 const log = console.log;
-export const app = express();
+const app = express();
 app.use('/', graphqlHTTP({
     schema: schema,  // Must be provided
     rootValue: root,
     graphiql: true,  // Enable GraphiQL when server endpoint is accessed in browser
 }));
 
-export default app;
+module.exports = app;
 
 
 
