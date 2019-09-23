2945cf557e9c7d8be7cd89139b59d8ac2e86452c
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2019-09-23 14:38:19 -0400

    gmaps.ks

---
+++ src/gmaps.js
@@ -0,0 +1,24 @@
+const googleMapsClient = require('@google/maps').createClient({
+    key:'AIzaSyBG5HdtAIB3OV-jC37XmrrgnW4T2Leep8E'
+});
+
+// class to interact with Map data
+module.exports = class Map {
+
+    constructor(){
+    }
+    // pos1,pos2 are of type class position (see position.js)
+    static getDistanceMatrix(pos1,pos2){
+
+    }
+
+    static getGeocodeFromAddr(str){
+
+    }
+
+    static getRutgersBuilding(){
+
+    }
+
+
+};
