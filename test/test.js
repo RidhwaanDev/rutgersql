50f50ffaa6591a8000e303850cdfc41915461f6b
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2019-10-23 12:31:00 -0400

    add tests

---
+++ test/test.js
@@ -0,0 +1,61 @@
+const assert = require('assert');
+const resolvers = require('../src/rootresolver');
+const routes = [];
+
+// test transloc API
+describe('transloc_api', () => {
+    describe('routes', () => {
+        it('should not return null for any routes',() => {
+            // TODO
+        });
+    });
+
+    describe('stops', () => {
+        it('should not return null for any stops',() => {
+            // TODO
+        });
+
+    });
+
+    describe('vehicles', () => {
+        it('should not return null for any vehicles',() => {
+            // TODO
+        });
+    });
+
+    describe('arrivals', () => {
+
+    });
+
+    describe('segments', () => {
+
+    });
+});
+
+// test complex resolvers
+describe('complex_resolvers', () => {
+    describe('getVehiclesByName', () => {
+
+    });
+
+    describe('getRoutesByName', () => {
+
+    });
+
+    describe('getSegmentsByName', () => {
+
+    });
+
+    describe('getStopsWithRoutes', () => {
+
+    });
+
+    describe('getNearbyStops', () => {
+
+    });
+});
+
+// test cache
+
+
+// test
