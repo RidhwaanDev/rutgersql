1b6387e9d66f3789b33771fc0268ac58b341dc5f
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2019-10-25 13:51:09 -0400

    dump heap snapshot

--- src/app.js
+++ src/app.js
@@ -5,6 +5,13 @@ const chalk  = require('chalk');
 const graphqlHTTP  = require("express-graphql");
 const schema  = require('./schema');
 const root  = require( './rootresolver');
+const heapdump = require('heapdump');
+setTimeout(() => {
+    heapdump.writeSnapshot(function(err, filename) {
+        console.log('dump written to', 'heapdump.heapsnapshot');
+    });
+
+}, 5000);
 const {queryMapsAPI} = require('./resolvers/directions');
 const app = express();
 app.use('/', graphqlHTTP({
