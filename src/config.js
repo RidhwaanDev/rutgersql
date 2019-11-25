570d7ba24638d52101418d48e029d782db00d807
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2019-11-25 13:13:42 -0500

    add more latlng test

--- src/config.js
+++ src/config.js
@@ -1,34 +1,38 @@
 module.exports = {
-     API_URL: 'https://transloc-api-1-2.p.rapidapi.com',
-     geo_area: '40.506831,-74.456645|15000',
-     stop_id_test : '4229492',
-     agency: 1323,
-     /** HOW TO PUT YOUR API_KEY
-      * First Way: Just put your API_KEY in directly
-      * Second Way: Create a file in the root directory called .env ( no name, just .env) and place your api key in the following format: API_KEY=YOUR_API_KEY  (no quotes)
-      */
-     HEADERS: {
-          'X-RapidAPI-Host': 'transloc-api-1-2.p.rapidapi.com',
-          'X-RapidAPI-Key': process.env.TRANSLOC_API_KEY,
-     },
+    API_URL: 'https://transloc-api-1-2.p.rapidapi.com',
+    geo_area: '40.506831,-74.456645|15000',
+    stop_id_test : '4229492',
+    agency: 1323,
+    /** HOW TO PUT YOUR API_KEY
+     * First Way: Just put your API_KEY in directly
+     * Second Way: Create a file in the root directory called .env ( no name, just .env) and place your api key in the following format: API_KEY=YOUR_API_KEY  (no quotes)
+     */
+    HEADERS: {
+        'X-RapidAPI-Host': 'transloc-api-1-2.p.rapidapi.com',
+        'X-RapidAPI-Key': process.env.TRANSLOC_API_KEY,
+    },
 
-     TRANSLOC_ANNOUCEMENTS: 'https://rutgers.transloc.com/announcements/rss',
-     RUTGERS_PLACES_API_URL: 'https://myapps.rutgers.edu/myr-services/RUBuildings/search/mdb/new%20brunswick',
-     GMAP_API_URL : 'https://maps.googleapis.com/maps/api/distancematrix/json',
-     GMAP_API_KEY: process.env.GMAP_API_KEY,
+    TRANSLOC_ANNOUCEMENTS: 'https://rutgers.transloc.com/announcements/rss',
+    RUTGERS_PLACES_API_URL: 'https://myapps.rutgers.edu/myr-services/RUBuildings/search/mdb/new%20brunswick',
+    GMAP_API_URL : 'https://maps.googleapis.com/maps/api/distancematrix/json',
+    GMAP_API_KEY: process.env.GMAP_API_KEY,
 
-     // test user_lat , user_lng . These coords are next to Buell apartment (which should be closest stop )
-     user_lat :   40.522847,
-     user_lng :  -74.455341,
+    // a bunch of coordinates to be copy and pasted into GraphiQL
+    latlngtest : {
+        // test user_lat , user_lng . These coords are next to Buell apartment (which should be closest stop )
+        buellapt_lat: 40.522847,
+        buellapt_lng: -74.455341,
+        // test user_lat 2, user_lng 2. Next to Hill center
+        hill_center_lat: 40.521926,
+        hill_center_lng: -74.462228,
 
-     // test user_lat 2, user_lng 2. Next to Hill center
-     user_lat_2 : 40.521926,
-     user_lng_2 : -74.462228,
+        // test dest_lat , dest_lng. These coords are next to Scott Hall (which should be closest stop )
+        scotth_lat: 40.499820,
+        scotth_lng: -74.448530,
 
-     // test dest_lat , dest_lng. These coords are next to Scott Hall (which should be closest stop )
-     dest_lat :40.499820,
-     dest_lng : -74.448530,
-
+        lsc_lat: 40.523907,
+        lsc_lng: -74.436665,
+    }
 };
 
 
