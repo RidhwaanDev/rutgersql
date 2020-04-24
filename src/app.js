6d6cc922f73521d593cb8f326ab5027b93c80a67
RidhwaanDev <ridhwaan.any@gmail.com>
2020-04-24 14:42:26 -0400

    add artillery.io for load testing

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
-const memwatch = require('node-memwatch');
+const memwatch = require('@airbnb/node-memwatch');
 const express  = require('express');
 const graphqlHTTP  = require("express-graphql"); // graphql server
 const schema  = require('./schema');
 const root  = require( './rootresolver');
 const app = express();
-const SocketAPI = require('./socketio');
 const http = require('http').Server(express); // socket.io server
 const io = require('socket.io')(http);
+
+// Wrapper class for socket.io
+const SocketAPI = require('./socketio');
+
 
 // // this port is used for socket.io testing
 const port = 3000;
@@ -20,11 +23,15 @@ app.use('/', graphqlHTTP({
 }));
 
 // if heap usage increases after five garbage collections then we probably have a leak.
+// for our purposes this should be all we need in terms of memory profiling. As long as there is no growing leaks, we should be good to go.
 memwatch.on('leak', (info) => {
+    for(let i = 0; i < 20; i++){
+        console.log("OH GOD THERE HAS BEEN A LEAK");
+    }
     console.log(info);
 });
 
-// called on every GC
+// called on every GC event
 memwatch.on('stats', (stats) => {
     console.log(stats);
  });
